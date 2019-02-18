dc91a41d4f18a32e8a23d979dc38acdb897a6bf6
Grant Cordle <granthc14@gmail.com>
2019-02-18 01:07:15 -0600

    added frontend and updated backend

--- src/main/java/com/granthc/levelsbeyondassessment/DatabaseController.java
+++ src/main/java/com/granthc/levelsbeyondassessment/DatabaseController.java
@@ -6,22 +6,23 @@ import java.util.Optional;
 
 public class DatabaseController {
 
+	// SQL statements
 	final static String SETUP_NOTE_TABLE = "CREATE TABLE IF NOT EXISTS NOTES (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, BODY TEXT NOT NULL);";
 	final static String CREATE_NOTE = "INSERT INTO NOTES (BODY) VALUES (?)";
 	final static String GET_LAST_ID = "SELECT last_insert_rowid()";
 	final static String GET_NOTE_WITH_ID = "SELECT * FROM NOTES WHERE ID = ?";
 	final static String GET_ALL_NOTES_WITH_QUERY = "SELECT * FROM NOTES WHERE BODY LIKE ?";
 	final static String UPDATE_NOTE_WITH_ID = "UPDATE NOTES SET BODY = ? WHERE ID = ?";
-	
-	/**
-	 * I don't really like adding the "throws exception" format, I'd rather handle
-	 * the exception, but in this case, I wanted to make sure that my connections
-	 * were closed in the finally block and didn't want to add another try / catch.
-	 * Can explain more if wanted.
-	 * 
-	 * @throws SQLException
-	 */
-	public static void establishDbConnection() throws SQLException {
+	final static String DELETE_NOTE_WITH_ID = "DELETE FROM NOTES WHERE ID = ?";
+	final static String DELETE_ALL_NOTES_WITH_QUERY = "DELETE FROM NOTES WHERE BODY LIKE ?";
+
+	// return strings
+	final static String NOTE_NOT_FOUND = "Note not found";
+	final static String NOTE_FOUND_AND_DELETED = "Note found and deleted";
+	final static String ALL_NOTES_DELETED = "All notes Deleted";
+	final static String NOTES_WITH_BODY_DELETED = "Notes with body deleted";
+
+	public static void establishDbConnection() {
 		Connection c = null;
 		Statement initialize = null;
 
@@ -35,12 +36,11 @@ public class DatabaseController {
 			initialize = c.createStatement();
 			initialize.executeUpdate(SETUP_NOTE_TABLE);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// close connections
 			initialize.close();
 			c.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -51,10 +51,9 @@ public class DatabaseController {
 		int newNoteId = -1;
 
 		try {
-			// setup db connection
+			// setup DB connection
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:levelsBeyond.db");
-			c.setAutoCommit(false);
 
 			// Insert the note using parameterized queries so that sql injection is not
 			// possible
@@ -69,11 +68,9 @@ public class DatabaseController {
 				newNoteId = rs.getInt("last_insert_rowid()");
 			}
 
-			// TODO: move these to a finally
 			rs.close();
 			insertStmt.close();
 			readStmt.close();
-			c.commit();
 			c.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -98,7 +95,10 @@ public class DatabaseController {
 			while (rs.next()) {
 				noteBody = rs.getString("body");
 			}
-			// TODO: Close everything
+
+			rs.close();
+			stmt.close();
+			c.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -131,35 +131,32 @@ public class DatabaseController {
 			while (rs.next()) {
 				notesToReturn.add(new Note(rs.getInt("id"), rs.getString("body")));
 			}
-// TODO: close things
+
+			rs.close();
+			stmt.close();
+			c.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		return notesToReturn;
 	}
-	
+
 	public static Note updateNote(int id, String updateBody) {
 		Connection c = null;
-		PreparedStatement insertStmt = null;
+		PreparedStatement stmt = null;
 
 		try {
-			// setup db connection
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:levelsBeyond.db");
-			c.setAutoCommit(false);
 
-			// Insert the note using parameterized queries so that sql injection is not
-			// possible
-			insertStmt = c.prepareStatement(UPDATE_NOTE_WITH_ID);
-			insertStmt.setString(1, updateBody);
-			insertStmt.setString(2,  Integer.toString(id));
-			insertStmt.executeUpdate();
-			
+			stmt = c.prepareStatement(UPDATE_NOTE_WITH_ID);
+			stmt.setString(1, updateBody);
+			stmt.setString(2, Integer.toString(id));
+			stmt.executeUpdate();
 
-			// TODO: move these to a finally
-			insertStmt.close();
-			c.commit();
+			stmt.close();
 			c.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -167,18 +164,68 @@ public class DatabaseController {
 
 		return new Note(id, updateBody);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public static String deleteNote(int id) {
+		Connection c = null;
+		PreparedStatement stmt = null;
+
+		Note requestedNote = getNote(id);
+		if (requestedNote == null)
+			return NOTE_NOT_FOUND;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:levelsBeyond.db");
+
+			stmt = c.prepareStatement(DELETE_NOTE_WITH_ID);
+			stmt.setString(1, Integer.toString(id));
+			stmt.execute();
+
+			stmt.close();
+			c.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return NOTE_FOUND_AND_DELETED;
+	}
+
+	public static String deleteAllNotes(Optional<String> query) {
+
+		Connection c = null;
+		PreparedStatement stmt = null;
+
+		ArrayList<Note> notesWithBody = getAllNotes(query);
+		if (notesWithBody.size() < 1)
+			return NOTE_NOT_FOUND;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:levelsBeyond.db");
+
+			stmt = c.prepareStatement(DELETE_ALL_NOTES_WITH_QUERY);
+
+			// add wildcards here because they aren't addable in the setString()
+			String queryString = "";
+			if (query.isPresent()) // check before get so we don't throw exception
+				queryString = "%" + query.get() + "%";
+			else
+				queryString = "%";
+
+			stmt.setString(1, queryString);
+			stmt.execute();
+
+			stmt.close();
+			c.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if (query.isPresent())
+			return NOTES_WITH_BODY_DELETED;
+		return ALL_NOTES_DELETED;
+	}
 
 }
--- src/main/java/com/granthc/levelsbeyondassessment/NotesController.java
+++ src/main/java/com/granthc/levelsbeyondassessment/NotesController.java
@@ -3,6 +3,8 @@ package com.granthc.levelsbeyondassessment;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,36 +16,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NotesController {
 
-	//Create a new Note
+	// Create a new Note
 	@RequestMapping(method = RequestMethod.POST, value = "/api/notes")
-	public Note postNote(@RequestBody Note note) {
-		return DatabaseController.saveNote(note.getBody());
+	public ResponseEntity<Note> postNote(@RequestBody Note note) {
+		return new ResponseEntity<Note>(DatabaseController.saveNote(note.getBody()), HttpStatus.CREATED);
 	}
 
-	//Get a Note with a specific ID
+	// Get a Note with a specific ID
 	@RequestMapping(method = RequestMethod.GET, value = "/api/notes/{noteId}")
-	public Note getNote(@PathVariable("noteId") int noteId) {
-		return DatabaseController.getNote(noteId);
+	public ResponseEntity<Note> getNote(@PathVariable("noteId") int noteId) {
+		Note foundNote = DatabaseController.getNote(noteId);
+		if (foundNote == null)
+			return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Note>(foundNote, HttpStatus.FOUND);
 	}
-	
-	//Get all notes + get all notes that contain a query in their body
+
+	// Get all notes + get all notes that contain a query in their body
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET, value = "/api/notes")
-	public ArrayList<Note> getAllNotes(@RequestParam("query") Optional<String> query) {
-		return DatabaseController.getAllNotes(query);
+	public ResponseEntity<ArrayList<Note>> getAllNotes(@RequestParam("query") Optional<String> query) {
+		return new ResponseEntity<ArrayList<Note>>(DatabaseController.getAllNotes(query), HttpStatus.FOUND);
 	}
-	
-	//Update a Note with ID
-	@RequestMapping(method = RequestMethod.POST, value = "/api/notes/{noteId}")
-	public Note updateNote(@PathVariable("noteId") int noteId, @RequestBody Note note) {
-		return DatabaseController.updateNote(noteId, note.getBody()); 
+
+	// Update a Note with ID
+	@RequestMapping(method = RequestMethod.PUT, value = "/api/notes/{noteId}")
+	public ResponseEntity<Note> updateNote(@PathVariable("noteId") int noteId, @RequestBody Note note) {
+		return new ResponseEntity<Note>(DatabaseController.updateNote(noteId, note.getBody()), HttpStatus.OK);
 	}
-	
-	//Update all Notes + update all notes that contain a certain body
-	
-	//Delete a Note with ID
-		
-	//Delete all Notes + delete all notes that contain a certain body
-	
+
+	// Delete a Note with ID
+	@RequestMapping(method = RequestMethod.DELETE, value = "/api/notes/{noteId}")
+	public ResponseEntity<Note> deleteNote(@PathVariable("noteId") int noteId) {
+		String result = DatabaseController.deleteNote(noteId);
+
+		if (result.equals(DatabaseController.NOTE_NOT_FOUND))
+			return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Note>(HttpStatus.NO_CONTENT);
+	}
+
+	// Delete all Notes + delete all notes that contain a certain body
+	@RequestMapping(method = RequestMethod.DELETE, value = "/api/notes")
+	public ResponseEntity<Note> deleteAllNotes(@RequestParam("query") Optional<String> query) {
+		String result = DatabaseController.deleteAllNotes(query);
+		if (result.equals(DatabaseController.NOTE_NOT_FOUND))
+			return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Note>(HttpStatus.NO_CONTENT);
+	}
 
 }
